ce917d6f81f42cb6fb4cb05d7e2d6a24765c8f3b
Simon K Zheng <simonkzheng@gmail.com>
2018-01-14 15:06:11 -0500

    Landing Page complete + Weather Page outline

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import { Component } from 'react';
 import Head from 'next/head';
-import HelloWorld from '../components/HelloWorld';
+import LandingPage from '../components/LandingPage';
+import WeatherPage from '../components/WeatherPage';
 
 export default class Page extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      zip: ""
+    }
+    this.requestWeather = this.requestWeather.bind(this);
+  }
   async getInitialProps() {
     return { };
+  }
+
+  requestWeather(zip){
+    console.log("HI FROM MOTHER", zip);
+    this.setState({ zip : zip });
   }
 
   render() {
@@ -13,7 +26,27 @@ export default class Page extends Component {
         <Head>
           <title>Take-home Project</title>
         </Head>
-        <HelloWorld />
+        <LandingPage requestWeather={this.requestWeather}/>
+        <WeatherPage zip={this.state.zip}/>
+        <style jsx global>{`
+          @font-face {
+            font-family: Apercu;
+            src: url('/static/fonts/ApercuRegularPro.otf');
+          }
+          @font-face {
+            font-family: Tiempos;
+            src: url('/static/fonts/Tiempos.otf');
+          }
+          body {
+            margin: 0;
+          }
+          h1 {
+            font-family: Tiempos;
+          }
+          p {
+            font-family: Apercu;
+          }
+        `}</style>
       </div>
     )
   }
